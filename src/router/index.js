de3386d5fed67ea83401cef1a130e7bcbc755af5
Clara Moreira <anacacamm@gmail.com>
2023-10-21 08:45:31 -0300

    adding routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import RecifeView from '../views/RecifeView.vue'
+import OlindaView from '../views/OlindaView.vue'
+import CaruaruView from '../views/CaruaruView.vue'
 import HomeView from '../views/HomeView.vue'
+import FernandoDeNoronhaView from '../views/FernandoDeNoronhaView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +12,26 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/recife',
+      name: 'recife',
+      component: RecifeView
+    },
+    {
+      path: '/olinda',
+      name: 'olinda',
+      component: OlindaView
+    },
+    {
+      path: '/caruaru',
+      name: 'caruaru',
+      component: CaruaruView
+    },
+    {
+      path: '/fernando-de-noronha',
+      name: 'fernando-de-noronha',
+      component: FernandoDeNoronhaView
     }
   ]
 })
